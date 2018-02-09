d5fd3e0f0db8de9450f9cb795b993f27397c21e4
Yee Wai <ywai@everspringpartners.com>
2018-02-09 11:58:17 -0600

    @wip using svg use instead of image

--- src/components/WorldsView/MapEditor/DrawMap.tsx
+++ src/components/WorldsView/MapEditor/DrawMap.tsx
@@ -4,7 +4,6 @@ import { ListGroup, ListGroupItem, Alert } from 'reactstrap';
 import moment from 'moment';
 import _ from 'lodash';
 
-
 import { OpenModalButton } from '@evercourse/ever-modal';
 
 import { StateTree } from 'services/types';
@@ -17,42 +16,46 @@ export type OwnProps  = {
 export const DrawMap: React.SFC<OwnProps> = (  { world } ) => {
     const TILE_SIZE = 100;
 
-    const board = _.times(world.width, _.constant(_.times(world.length, _.constant(null))));
+    const board = _.times(10, _.constant(_.times(10, _.constant(null))));
 
     const TILE_WIDTH_HALF = TILE_SIZE/2;
     const TILE_HEIGHT_HALF = (TILE_SIZE/4) + (TILE_SIZE * 0.04); //The 0.04 is to offset the ground having a thickness
 
     const TILE_HEIGHT = TILE_SIZE*4;
-
     return (
-        <svg className="map" width="100%" height="800" viewBox={`${-TILE_SIZE*10/2} ${TILE_HEIGHT} ${TILE_SIZE*10} ${TILE_SIZE*10}`} preserveAspectRatio="xMidYMid">
-        { board.map( (row, x) => (
-            row.map( (col, y) => {
-                const definition = world.structures[Math.floor(Math.random() * 4)].definition;
-                const ratio =  TILE_SIZE/definition.imageWidth;
+        <section>
 
-                const definitionImage = definition.image;
-                console.log(definitionImage)
+            <svg className="map" width="100%" height="800" viewBox={`${-TILE_SIZE*10/2} ${TILE_HEIGHT*1.5} ${TILE_SIZE*10} ${TILE_SIZE*10}`} preserveAspectRatio="xMidYMid">
+            <defs className="structureReference">
+            { world.structures.map( s => (
+                <svg id={s.definition.id} key={s.definition.id}
+                width={ TILE_SIZE }
+                viewBox={`0 0 ${s.definition.imageWidth} ${s.definition.imageHeight}`}
+                dangerouslySetInnerHTML={{__html: s.definition.image.innerHTML}}
+                fill="currentcolor"
+                />
+            ))}
+            </defs>
+            { board.map( (row, x) => (
+                row.map( (col, y) => {
+                    const definition = world.structures[Math.floor(Math.random() * 4)].definition;
+                    const ratio =  TILE_SIZE/definition.imageWidth;
 
-                return (<svg className="tile"
-                    key={x*1000 + y}
-                    x={ x * TILE_WIDTH_HALF - y * TILE_WIDTH_HALF }
-                    y={ (x * TILE_HEIGHT_HALF + y * TILE_HEIGHT_HALF)}
-                    width={TILE_SIZE} height={TILE_HEIGHT}
-                    viewBox={`0 0 ${TILE_SIZE} ${TILE_HEIGHT}`}
-                >
+                    return (
 
-                    <image
-                        y={TILE_HEIGHT - (definition.imageHeight * ratio)}
-                        xlinkHref={`${definition.imageUrl}`}
-                        width={ TILE_SIZE }
-                    />
+                        <use className="tile"
+                            key={x*1000 + y}
+                            x={ x * TILE_WIDTH_HALF - y * TILE_WIDTH_HALF }
+                            y={ (x * TILE_HEIGHT_HALF + y * TILE_HEIGHT_HALF) + ((TILE_HEIGHT - (definition.imageHeight * ratio))/2) }
+                            xlinkHref={`#${definition.id}`}
+                            fill="yellow"
+                        />
 
-
-                 </svg>)
-            })
-        )) }
-        </svg>
+)
+                })
+            )) }
+            </svg>
+        </section>
     )
 };
 
